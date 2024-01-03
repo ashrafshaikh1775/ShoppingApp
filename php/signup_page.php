db201e00223629d24fb1f568f60cb8c8cd9b6bb9
Ashraf Shaikh <ashrafshaikh1775@gmail.com>
2024-01-03 14:13:15 +0530

    changing names of signup_page

--- php/signup_page.php
+++ php/signup_page.php
@@ -6,20 +6,20 @@
     <meta name="viewport" content="width=device-width ,initial-scale=1.0" charset="utf-8">
 </head>
 <body>
-	<div class='login_bg'>
-     <div class="login_form">
+	<div class='signup_bg'>
+     <div class="signup_form">
      	<form action="" method="POST">
-     		<input type="text" name="username" class="login_txt" placeholder="username"></input>
-     		<input type="text" name="password" class="login_txt"placeholder="password"></input>
-     		<input type="email" name="email" class="login_txt"placeholder="email"></input>
-     	 	<input type="number" name="number" class="login_txt"placeholder="mobile"></input>
-     	 	<textarea id="address" name="address" class="login_txt" placeholder="address"></textarea></br></br>
+     		<input type="text" name="username" class="signup_txt" placeholder="username"></input>
+     		<input type="text" name="password" class="signup_txt"placeholder="password"></input>
+     		<input type="email" name="email" class="signup_txt"placeholder="email"></input>
+     	 	<input type="number" name="number" class="signup_txt"placeholder="mobile"></input>
+     	 	<textarea id="address" name="address" class="signup_txt" placeholder="address"></textarea></br></br>
      	 	
      	   <input type="radio" name="radio" class="radio" value="male">Male</input>
      	   <input type="radio" name="radio" class="radio" value="female">Female</input>
      	   <input type="radio" name="radio" class="radio" value="other">Other</input>
 
-     		<input type="submit" name="btnsub" class="login_btn"value="SignIn"></input>
+     		<input type="submit" name="btnsub" class="signup_btn"value="SignUp"></input>
      	   </br></br>
      		<a href="login_page.php">LogIn<a/>
      	</form>
